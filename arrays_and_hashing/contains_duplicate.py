42975636c35e905e1f1ffd11d8a50ff158f74dca
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-05-18 13:14:44 -0400

    Initial commit

--- arrays_and_hashing/contains_duplicate.py
+++ arrays_and_hashing/contains_duplicate.py
@@ -1,4 +1,4 @@
 #LeetCode 217. Contains Duplicate
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(self, nums) -> bool: #nums: List[int]
         return len(set(nums)) != len(nums)
