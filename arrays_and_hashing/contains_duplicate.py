f5719a85fc974fc5abdc74dce3927aaf9ac9f3a0
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-05-20 12:17:54 -0400

    Initial Commit

--- arrays_and_hashing/contains_duplicate.py
+++ arrays_and_hashing/contains_duplicate.py
@@ -1,4 +1,4 @@
-#LeetCode 217. Contains Duplicate
+#217. Contains Duplicate
 class Solution:
     def containsDuplicate(self, nums) -> bool: #nums: List[int]
         return len(set(nums)) != len(nums)
---
+++ two_pointers/two_sum_ii.py
@@ -0,0 +1,13 @@
+#167. Two Sum II - Input Array Is Sorted
+class Solution:
+    def twoSum(self, numbers, target: int): #numbers: List[int]
+        # No hints
+        left = 0
+        right = len(numbers) - 1
+        while numbers[left] + numbers[right] != target:
+            while numbers[left] + numbers[right] < target:
+                left += 1
+            while numbers[left] + numbers[right] > target:
+                right -= 1
+
+        return [left+1, right+1]
