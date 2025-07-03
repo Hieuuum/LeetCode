ce4814110ef6cd5a5ee0d602d49ec9dcbf440675
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-07-03 12:06:11 -0400

    Add 1 ll

---
+++ linked_list/reverse_ll.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head or not head.next:
+            return head
+            
+        cur = head
+        before = None
+        after = cur.next
+
+        while cur:
+            after = cur.next
+            cur.next = before
+            
+            before = cur
+            cur = after
+
+        head = before
+
+        return head
