fe8f2dc30aabfae5fc61f34b3ccd377d3d8da1e2
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-07-09 11:52:42 -0400

    Add 1 BT

---
+++ binary_tree/mps_bt.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = float('-inf')
+        
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return 0
+
+            cur = root.val
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            extendableSum = max(cur, cur + left, cur + right)
+            maxSum = max(extendableSum, cur + left + right)
+            if res < maxSum:
+                res = maxSum 
+            
+            return extendableSum
+        
+        dfs(root)
+        return res
