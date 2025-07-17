9f3c9a4e9ecc6fb6c66fcd5ad106146f00219007
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-07-17 10:37:11 -0400

    Fix 1 graph

--- graph/numIslands.py
+++ graph/numIslands.py
@@ -1,7 +1,7 @@
 # 200. Number of Islands (Medium)
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         ROWS, COLS = len(grid), len(grid[0])
         count = 0
         
@@ -11,10 +11,8 @@ class Solution:
                 return
             
             grid[r][c] = "0"
-            dfs(r, c+1)
-            dfs(r, c-1)
-            dfs(r+1, c)
-            dfs(r-1, c)
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
         
         for r in range(ROWS):
             for c in range(COLS):
@@ -23,3 +21,4 @@ class Solution:
                     dfs(r, c)
         
         return count
+
