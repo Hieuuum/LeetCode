32e09379e61ec5ba6ceff946b87d42c9eee34292
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-07-21 13:55:43 -0400

    Fix 1 graph

--- graph/crs_shed_ii.py
+++ graph/crs_shed_ii.py
@@ -28,3 +28,38 @@ class Solution:
                     res.append(c)
         
         return res if takenCourses == numCourses else []
+    
+# 3-State DFS
+
+# class Solution:
+#     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+#         adj = {c:[] for c in range(numCourses)}
+#         # 0: univisited, 1: visiting, 2:visited
+#         visitState = [0] * numCourses
+#         res = []
+
+#         for course, req in prerequisites:
+#             adj[course].append(req)
+        
+#         def hasCycle(c):
+#             if visitState[c] == 2:
+#                 return False
+#             elif visitState[c] == 1:
+#                 return True
+            
+#             visitState[c] = 1
+
+#             for req in adj[c]:
+#                 if hasCycle(req):
+#                     return True
+            
+#             visitState[c] = 2
+#             res.append(c)
+#             return False
+        
+#         for c in range(numCourses):
+#             if visitState[c] == 0:
+#                 if hasCycle(c):
+#                     return []
+        
+#         return res
