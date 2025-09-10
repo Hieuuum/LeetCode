bc5bce05cd71c1acad5669ef5e225e233af24f1b
Hiếu Nguyễn Viết Minh <hieu.vm.nguyen@gmail.com>
2025-09-09 20:45:13 -0400

    Add 1 DP

---
+++ dynamic_programming/max_prod_subarr.py
@@ -0,0 +1,19 @@
+# 152. Maximum Product Subarray (Medium)
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMin, curMax = 1, 1
+
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            
+            tmp = n * curMax
+            curMax = max(tmp, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+
+            res = max(res, curMax)
+        
+        return res
